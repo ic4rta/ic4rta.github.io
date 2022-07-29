3b26a80109b380248a62ffa7a16a65f7588b85ca
c4rta <98796947+ic4rta@users.noreply.github.com>
2022-07-29 17:44:14 -0500

    Add files via upload

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -9,8 +9,13 @@ var store = [{
         "tags": ["formatStr","radare2"],
         "url": "http://localhost:4000/format-string/",
         "teaser":null},{
+        "title": "picoCTF - Hurry up! Wait!",
+        "excerpt":"Contenido Sacando informacion del binario. Debugging al binario con radare2. Sacando informacion del binario Lo primero que vemos a la hora de descargar el binario de la pagina de picoCTF es un binario con el nombre svchost.exe, con ese nombre uno pensaria que es un binario .exe para ser ejecutado...","categories": ["Reversing"],
+        "tags": ["radare2"],
+        "url": "http://localhost:4000/picoCTF-hurryUp/",
+        "teaser":null},{
         "title": "ret2libc bypass NX",
-        "excerpt":"Contenido ¿Que es ret2libc?. ¿Que es No Executable (NX)?. Un poco de Return Oriented Programming (ROP). Analizando nuestro binario. Explicando el payload que usaremos. Desbordando el buffer y calculando el offset del RIP. Consiguiendo la direccion de libc. Consiguiendo los offset de RET, POP RDI RET, /bin/sh y system(). Creando...","categories": ["Explotacion binaria"],
+        "excerpt":"Contenido ¿Que es ret2libc?. ¿Que es No Executable (NX)?. Un poco de Return Oriented Programming (ROP). Analizando nuestro binario. Explicando el payload que usaremos. Desbordando el buffer y calculando el offset del RIP. Consiguiendo la direccion de libc. Consiguiendo los offset de ret, pop rdi ret, /bin/sh y system(). Creando...","categories": ["Explotacion binaria"],
         "tags": ["ret2libc","gdb","bof"],
         "url": "http://localhost:4000/ret2libc/",
         "teaser":null}]
