ca2e67861e54a597d230c0b65a0d2ad609202fdd
c4rta <98796947+ic4rta@users.noreply.github.com>
2022-07-28 19:10:13 -0500

    Add files via upload

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "BoF stack-based",
+        "excerpt":"Contenido ¿Que es buffer overflow?. Layout del stack. Analisis el codigo fuente del binario Desbordando el buffer. Calculando el offset y tomando el control del RIP. Ejecutando la shellcode. ¿Que es buffer overflow? El buffer es un espacio en la memoria en el cual se almacenan datos de manera temporal...","categories": ["Explotacion binaria"],
+        "tags": ["bof","pwndbg"],
+        "url": "http://localhost:4000/bof-stack-based/",
+        "teaser":null}]
