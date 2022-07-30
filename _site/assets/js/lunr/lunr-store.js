8d82b60491502c09c12528a79aaccee42e0068dd
c4rta <98796947+ic4rta@users.noreply.github.com>
2022-07-30 14:50:04 -0500

    Add files via upload

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -10,7 +10,7 @@ var store = [{
         "url": "http://localhost:4000/format-string/",
         "teaser":null},{
         "title": "picoCTF - Hurry up! Wait!",
-        "excerpt":"Contenido Sacando informacion del binario. Debugging al binario con radare2. Sacando informacion del binario Lo primero que vemos a la hora de descargar el binario de la pagina de picoCTF es un binario con el nombre svchost.exe, con ese nombre uno pensaria que es un binario .exe para ser ejecutado...","categories": ["Reversing"],
+        "excerpt":"Contenido Sacando informacion del binario. Debugging al binario con radare2. Sacando informacion del binario Lo primero que vemos a la hora de descargar el binario de la pagina de picoCTF es un binario con el nombre svchost.exe, con ese nombre uno pensaria que es un binario .exe para ser ejecutado...","categories": ["picoCTF","Reversing"],
         "tags": ["radare2"],
         "url": "http://localhost:4000/picoCTF-hurryUp/",
         "teaser":null},{
@@ -18,4 +18,9 @@ var store = [{
         "excerpt":"Contenido ¿Que es ret2libc?. ¿Que es No Executable (NX)?. Un poco de Return Oriented Programming (ROP). Analizando nuestro binario. Explicando el payload que usaremos. Desbordando el buffer y calculando el offset del RIP. Consiguiendo la direccion de libc. Consiguiendo los offset de ret, pop rdi ret, /bin/sh y system(). Creando...","categories": ["Explotacion binaria"],
         "tags": ["ret2libc","gdb","bof"],
         "url": "http://localhost:4000/ret2libc/",
+        "teaser":null},{
+        "title": "GOT overwrite con Format String",
+        "excerpt":"Contenido Global Offset Table (GOT) y Procedure Linked Table (PLT) Analisis del binario Calculando el offset del buffer y la direccion de libc Armando nuestro exploit GOT y PLT Para entender esto mejor veremos la estructura de un binario ELF. Ahora pondre otra estructura donde section header table apunta a...","categories": ["Explotacion binaria"],
+        "tags": ["got-plt","gdb","formatStr"],
+        "url": "http://localhost:4000/got-overwrite/",
         "teaser":null}]
